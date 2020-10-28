ebe91f982e75a61f3aec757296601090a3b0f699
ttdp <tiantong.dev@gmail.com>
2020-10-28 13:31:22 +0800

    Initial commit

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class FilterableProductTable extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Thinking in React</h1>
+            </div>
+        )
+    }
+}
+
+export default FilterableProductTable
